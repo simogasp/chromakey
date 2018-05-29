aa770efe8760c35777f8ab2a2afb4cb9956b5a23
Simone Gasparini <simone.gasparini@gmail.com>
2018-05-29 16:52:30 +0200

    [video] added alpha blending with a background image

--- mainVideo.cpp
+++ mainVideo.cpp
@@ -9,7 +9,7 @@
 
 void usage(const char* argv)
 {
-    std::cout << "Usage:\n " << argv[0] << " <filename> " << "\n";
+    std::cout << "Usage:\n " << argv[0] << " <filename> [<background>] " << "\n";
 }
 
 int main(int argc, char* argv[] )
@@ -24,13 +24,22 @@ int main(int argc, char* argv[] )
         return EXIT_FAILURE;
     }
 
+    bool withBackground = false;
+    if(argc == 2)
+    {
+        withBackground = true;
+    }
+
+    cv::Mat background;
+
+
     const std::string ORIGINAL_IMG = "original image";
     const std::string OUTPUT_IMG = "mask image";
     const std::string FINAL_IMG = "final image";
 
     int delay = 10;
 
-    cv::namedWindow(FINAL_IMG,1);
+    cv::namedWindow(FINAL_IMG, cv::WINDOW_NORMAL);
     while(true)
     {
         cv::Mat frame;
@@ -39,14 +48,33 @@ int main(int argc, char* argv[] )
         if(frame.empty())
             break;
 
+        if(withBackground && background.empty())
+        {
+            background = cv::imread(std::string(argv[1]), cv::IMREAD_COLOR);
+
+            // resize if the background has not the same size
+            if(background.rows != frame.rows && background.cols != frame.cols)
+            {
+                cv::Mat temp(frame.rows, frame.cols, background.type());
+                cv::resize(background, temp, cv::Size(frame.cols, frame.rows));
+                background = temp;
+            }
+        }
+
         cv::Mat outMask;
         cv::Mat outImage;
 
         computeChromaMask(frame, outImage, outMask);
 
-        outImage = applyTransparencyMask(outImage, outMask);
+//        outImage = applyTransparencyMask(outImage, outMask);
 
+        if(withBackground)
+        {
+            assert(!background.empty());
+            outImage = alphaBlending(outImage, background, outMask);
+        }
         cv::imshow(FINAL_IMG, outImage);
+
         if( cv::waitKey(delay) == 27 )
             break;
         const char key = (char) cv::waitKey(delay);
