2f87c0c0bb53eb573a49df00153a5feb99b455b2
Simone Gasparini <simone.gasparini@gmail.com>
2018-05-29 16:51:36 +0200

    [main] added blending with a background image

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@
 
 void usage(const char* argv)
 {
-    std::cout << "Usage:\n " << argv[0] << " <filename> " << "\n";
+    std::cout << "Usage:\n " << argv[0] << " <image> [<background>] " << "\n";
 }
 
 int main(int argc, char* argv[] )
 {
-    if(argc != 2)
+    if(argc <= 2 || argc > 3)
     {
         usage(argv[0]);
     }
@@ -25,11 +25,12 @@ int main(int argc, char* argv[] )
 
     computeChromaMask(rgbImg, outImage, outMask);
 
-    outImage = applyTransparencyMask(outImage, outMask);
+    cv::Mat transparent = applyTransparencyMask(outImage, outMask);
 
     const std::string ORIGINAL_IMG = "original image";
     const std::string OUTPUT_IMG = "mask image";
     const std::string FINAL_IMG = "final image";
+    const std::string BLENDED_IMG = "blended image";
 
     cvNamedWindow(ORIGINAL_IMG.c_str());
     cv::imshow(ORIGINAL_IMG, rgbImg);
@@ -40,10 +41,33 @@ int main(int argc, char* argv[] )
     cv::waitKey(-1);
 
     cvNamedWindow(FINAL_IMG.c_str());
-    cv::imshow(FINAL_IMG, outImage);
+    cv::imshow(FINAL_IMG, transparent);
     cv::waitKey(-1);
 
-    cv::imwrite("result.png", outImage);
+    cv::imwrite("result.png", transparent);
+
+    if(argc != 3)
+    {
+        return EXIT_SUCCESS;
+    }
+
+    cv::Mat background = cv::imread(std::string(argv[2]), cv::IMREAD_COLOR);
+
+    // resize if the background has not the same size
+    if(background.rows != outImage.rows && background.cols != outImage.cols)
+    {
+        cv::Mat temp(outImage.rows, outImage.cols, background.type());
+        cv::resize(background, temp, cv::Size(outImage.cols, outImage.rows));
+        background = temp;
+    }
+
+    cv::Mat blended = alphaBlending(outImage, background, outMask);
+
+    cvNamedWindow(BLENDED_IMG.c_str());
+    cv::imshow(BLENDED_IMG, blended);
+    cv::waitKey(-1);
+
+    cv::imwrite("blended.png", blended);
 
     return EXIT_SUCCESS;
 }
